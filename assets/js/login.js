082e44cc64a46f0506a7c39ddefdbca3566a7513
PatriciaSouza1976 <patriciadesouza.sz@gmail.com>
2026-05-07 21:33:45 -0300

    Aula

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,7 @@
-const email = "admin@exemplo.com";
-const senha = "1234";
+import { usuario } from "./bd.js";
+
+const email = usuario.email;
+const senha = usuario.senha;
 
 function verificarcredenciais() {
     const emailInput = document.getElementById("email").value;
@@ -9,9 +11,17 @@ function verificarcredenciais() {
         alert("Email correto!");
         if (senhaInput === senha) {
             alert("Login bem-sucedido!");
-            window.location.href = "index.html";
+            window.location.href = "home.html";
         } else
             alert("Senha incorreta!");
     } else
         alert("Email informado incorretamente");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const botao = document.querySelector("button");
+    botao.addEventListener("click", (e) => {
+        e.preventDefault();
+        verificarcredenciais();
+    });
+});
