e4a2e1fe7f9ce5391aa2fc31509e47c068180406
Patricia de Souza <62427@sga.pucminas.br>
2026-05-28 22:02:10 -0300

    2

--- assets/js/detalhe.js
+++ assets/js/detalhe.js
@@ -8,19 +8,20 @@ const  id = parseInt(params.get("id"));
 const noticia = dados_das_noticias.find(noticia => noticia.id === id);
 
 if (noticia) {
-    const params = new URLSearchParams(window.location.search);
-const  id = parseInt(params.get("id"));
-
-const noticia = dados_das_noticias.find(noticia => noticia.id === id);
-
-if (noticia) {
 
     container.innerHTML = `
     <h1 class="titulo-detalhe">${noticia.titulo}</h1>
     <p><strong>${noticia.categoria}</strong> - ${noticia.data} - por ${noticia.autor}</p>
     <img src="${noticia.imagem}" alt="${noticia.titulo}" class="painel-img">
+    <p>${noticia.data}</p>
+    <p class="descrição-da-noticia">${noticia.descricao}</p>
+    <p >${noticia.conteudo}</p>
+    <p><a href="./home.html">Voltar para Home</a></p>
 
 
 }
-    
- 
+    else {
+
+container.innerHTML = "<p>Notícia não encontrada.</p>";
+
+}
